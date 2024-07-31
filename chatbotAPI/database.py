d51e96366dd63645f179dbee88883b7ce4c77f5c
Destin  Niyomufasha <dniyomufasha@ppg.com>
2024-07-31 13:41:56 -0400

    with chatbot api

--- chatbotAPI/database.py
+++ chatbotAPI/database.py
@@ -1,6 +1,1 @@
-from pymongo import MongoClient 
 
-def get__db_handle(name, host, port, username, password):
-    client = MongoClient(host = host, port = int(port), username = username, password(password))
-    handle = client("database_name")
-    return handle, client
