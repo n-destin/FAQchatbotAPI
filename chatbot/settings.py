6aa4f298efe8327c11e0cc992de8d73a3e4688c7
Destin  Niyomufasha <dniyomufasha@ppg.com>
2024-08-06 15:07:57 -0400

    removing djongo

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     'rest_framework_simplejwt',
     'chatbotAPI.apps.ChatbotapiConfig',
-    'djongo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
